2fa526e543abd94e71f4b5b1cb414ef1e10f5f98
William Sjafri <sjafriwilliam@gmail.com>
2020-10-07 21:47:15 -0700

    done practice

---
+++ OneDrive/Desktop/repos/CSharpPractice/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace PracticewithVariables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 17; //numerical value
+            bool conditional = true; // true or false value
+            char letter = 'S'; //single value
+            string firstName = "William"; //combination of values
+            //can't change data type. every variable must be set to a data type
+            double num = 5.8; //decimals
+            decimal X = 178.22M; //decimals
+            Console.WriteLine($"My name is {firstName} {letter} I am {number} years old") ;
+     
+            Console.WriteLine($"I am {num} inches or {X} cm and all of this is {conditional}");
+        }
+    }
+}
